6e7a726d9cdb4838aa0a97cbf6af3666c4b8cbb2
Herbert Ilhan Tanujaya <mail@herbert.id>
2017-10-31 21:14:26 +0800

    Storage!

--- popup/index.js
+++ popup/index.js
@@ -2,13 +2,21 @@ function disableHeader() {
   browser.tabs.insertCSS(null, {
     code: '._3YewW:not(._1UvQg):not(.MwhOx) { display: none; }'
   });
+
+  browser.storage.local.set({ disableHeader: true });
 }
 
 function enableHeader() {
   browser.tabs.removeCSS(null, {
     code: '._3YewW:not(._1UvQg):not(.MwhOx) { display: none; }'
   });
+
+  browser.storage.local.set({ disableHeader: false });
 }
+
+browser.storage.local.get().then((item) => {
+  document.getElementById('disable-header').checked = item.disableHeader;
+});
 
 document.getElementById('disable-header').addEventListener('click', (e) => {
   e.target.checked ? disableHeader() : enableHeader();
