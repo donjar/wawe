3e7d5fdcf979cdc3e32820637ec992b18d3c752b
Herbert Ilhan Tanujaya <mail@herbert.id>
2017-10-31 22:29:47 +0800

    refactor

--- popup/index.js
+++ popup/index.js
@@ -1,23 +1,55 @@
-function disableHeader() {
-  browser.tabs.insertCSS(null, {
-    code: '._3YewW:not(._1UvQg):not(.MwhOx) { display: none; }'
-  });
+class Wawe {
+  constructor() {
+    this.storage = browser.storage.local.get();
 
-  browser.storage.local.set({ disableHeader: true });
+    for (const methodName of ['disableHeader']) {
+      this.bindState(methodName);
+    }
+  }
+
+  /**
+   * Does two things:
+   * 1. Gets the saved state from storage and applies it to the checkbox.
+   * 2. Makes the checkbox listen to click events and act accordingly.
+   */
+  bindState(checkboxId) {
+    const elem = document.getElementById(checkboxId);
+
+    this.storage.then((item) => {
+      elem.checked = item[checkboxId];
+    });
+
+    elem.addEventListener('click', (e) => {
+      this.act(checkboxId, e.target.checked);
+    });
+  }
+
+  /**
+   * Runs the method associated with the event, and sets the local storage.
+   */
+  act(eventName, eventValue) {
+    const methodName = this.getMethodName(eventName);
+    this[methodName](eventValue);
+
+    browser.storage.local.set({ [eventName]: eventValue });
+  }
+
+  /**
+   * Gets the method name to be run from the event name.
+   */
+  getMethodName(eventName) {
+    return 'set' + eventName.charAt(0).toUpperCase() + eventName.substring(1);
+  }
+
+  setDisableHeader(disable) {
+    const cssCode = '._3YewW:not(._1UvQg):not(.MwhOx) { display: none; }';
+
+    if (disable) {
+      browser.tabs.insertCSS(null, { code: cssCode });
+    } else {
+      browser.tabs.removeCSS(null, { code: cssCode });
+    }
+  }
 }
 
-function enableHeader() {
-  browser.tabs.removeCSS(null, {
-    code: '._3YewW:not(._1UvQg):not(.MwhOx) { display: none; }'
-  });
-
-  browser.storage.local.set({ disableHeader: false });
-}
-
-browser.storage.local.get().then((item) => {
-  document.getElementById('disable-header').checked = item.disableHeader;
-});
-
-document.getElementById('disable-header').addEventListener('click', (e) => {
-  e.target.checked ? disableHeader() : enableHeader();
-});
+new Wawe();
